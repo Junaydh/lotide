b01dc3367b1974af439a806a747c042c06930df7
Junayd H <junaydhus@gmail.com>
2023-01-11 13:37:58 -0800

    implemented function to return the 1 or 2 middle elements of an array

---
+++ middle.js
@@ -0,0 +1,10 @@
+const middle = function(array) {
+  let result = [];
+  if (array.length % 2 === 0) {
+    result.push(array[array.length / 2 - 1]);
+    result.push(array[array.length / 2]);
+  } else {
+    result.push(array[Math.ceil(array.length / 2 - 1)]);
+  }
+  return result;
+};
