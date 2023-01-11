f43d15c1231b5393486ed97af4222718c89fcd6c
Junayd H <junaydhus@gmail.com>
2023-01-11 12:23:33 -0800

    implemented a function to remove specific unwanted elements from an array

---
+++ without.js
@@ -0,0 +1,18 @@
+const existsIn = function(element, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (element === array[i]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const without = function(array, without) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (existsIn(array[i], without) === false) {
+      result.push(array[i])
+    }
+  }
+  return result;
+};
