df7e4ddbc4540962516d4af1eb6b02967c60c0dc
Junayd H <junaydhus@gmail.com>
2023-01-12 11:45:31 -0800

    implemented a function to count specific values in an array and output an object with these values as key:value pairs

---
+++ countOnly.js
@@ -0,0 +1,34 @@
+const countOnly = function(allItems, itemsToCount) {
+  let counted = {};
+  for (const property in itemsToCount) {
+    if (itemsToCount[property] === true) {
+      counted[property] = 0;
+      for (let i = 0; i < allItems.length; i++) {
+        if (allItems[i] === property) {
+          counted[property] += 1;
+        }
+      }
+    }
+    for (const property in counted) {
+      if (counted[property] === 0) {
+        delete counted[property];
+      }
+    }
+  }
+  return counted;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+console.log(result1);
