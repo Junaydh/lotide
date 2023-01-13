d716137fa840dcac1064508d8c8b83e1cc1b67ad
Junayd H <junaydhus@gmail.com>
2023-01-12 16:55:35 -0800

    sister function to console.log a more visible message

---
+++ assertObjectsEqual.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (const property in obj1) {
+    if (Array.isArray(obj1[property]) && Array.isArray(obj2[property])) {
+      if (!(eqArrays(obj1[property], obj2[property]))) {
+        return false;
+      }
+    } else if (!(obj1[property] === obj2[property])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(obj1, obj2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(obj1, obj2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`🚩🚩🚩 Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
+  }
+};
