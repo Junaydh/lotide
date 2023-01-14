5cdeec48d0d0bbd72a700259e7b678b7f039103a
Junayd H <junaydhus@gmail.com>
2023-01-14 10:49:06 -0800

    implemented a function to return a slice of an array from the beginning until it fails the provided comparison

---
+++ takeUntil.js
@@ -0,0 +1,10 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i])) {
+      results.push(array[i]);
+    } else {
+      return results;
+    }
+  }
+};
