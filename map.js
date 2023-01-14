775586017e10b50782b99ceb4a05997aaacd8713
Junayd H <junaydhus@gmail.com>
2023-01-14 10:50:14 -0800

    implemented a basic version of the .map method

---
+++ map.js
@@ -0,0 +1,8 @@
+const map = function(array, callback) {
+  const results = [];
+  for (let i = 0; i < array.length; i++) {
+    results.push(callback(array[i]));
+    console.log(i);
+  }
+  return results;
+};
