c8873dc2358d18bb96832954cd287e789c248028
Junayd H <junaydhus@gmail.com>
2023-01-15 19:59:23 -0800

    implemented function to flatten nested arrays into one

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(arr) {
+  let flat = [];
+  for (const ele of arr) {
+    if (Array.isArray(ele)) {
+      for (const subEle of ele) {
+        flat.push(subEle);
+      }
+    } else {
+      flat.push(ele);
+    }
+  }
+  return flat;
+};
